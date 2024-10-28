d6177f2aa7db353f4436a80786ac516a0cfdf5cb
L0rd-Inquisit0r <sean.s.l.duran@gmail.com>
2024-10-28 14:13:40 +0800

    defining the structure of data

--- solitaire.c
+++ solitaire.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 
+//Defining structure of a card
+typedef struct card{
+    int val;
+    int suit;
+}Card;
+
+//Using array stack implementation to define a deck of cards
+typedef struct deck{
+    Card *cards;
+    int size;
+}Deck;
+
+//Defining structure of play table
+typedef struct table{
+    Deck shuffleDeck[2];
+    Deck playDeck[7];
+    Deck acesDeck[4];
+}Table;
+
 int main(){
 
     return 0;
